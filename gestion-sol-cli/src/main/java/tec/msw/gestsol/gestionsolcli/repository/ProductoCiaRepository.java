e43cdb4b62009259a6facbaf2d27da6fd3560499
jdicao <105899704+jdicao@users.noreply.github.com>
2024-01-28 18:48:09 -0500

    Actualizacion

--- gestion-sol-cli/src/main/java/tec/msw/gestsol/gestionsolcli/repository/ProductoCiaRepository.java
+++
@@ -1,8 +0,0 @@
-package tec.msw.gestsol.gestionsolcli.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tec.msw.gestsol.gestionsolcli.entity.ProductoCia;
-
-public interface ProductoCiaRepository extends JpaRepository<ProductoCia, Long> {
-
-}
--- gestion-sol-cli/src/main/java/tec/msw/gestsol/gestionsolcli/repository/TipoSolicitudRepository.java
+++
@@ -1,7 +0,0 @@
-package tec.msw.gestsol.gestionsolcli.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tec.msw.gestsol.gestionsolcli.entity.TipoSolicitud;
-
-public interface TipoSolicitudRepository extends JpaRepository<TipoSolicitud, Long> {
-}
